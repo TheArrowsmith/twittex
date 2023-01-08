47127770caeca38ea9767167c75bb3993cdea3ce
George <george@arrowsmith.codes>
2023-01-08 18:01:21 +0000

    allow login with username or email

--- lib/twittex_web/controllers/user_session_controller.ex
+++ lib/twittex_web/controllers/user_session_controller.ex
@@ -9,13 +9,13 @@ defmodule TwittexWeb.UserSessionController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    %{"email" => email, "password" => password} = user_params
+    %{"email_or_password" => email_or_password, "password" => password} = user_params
 
-    if user = Accounts.get_user_by_email_and_password(email, password) do
+    if user = Accounts.get_authenticated_user(email_or_password, password) do
       UserAuth.log_in_user(conn, user, user_params)
     else
-      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
-      render(conn, "new.html", error_message: "Invalid email or password")
+     # In order to prevent user enumeration attacks, don't disclose whether the email or username is registered.
+      render(conn, "new.html", error_message: "Invalid login")
     end
   end
 
